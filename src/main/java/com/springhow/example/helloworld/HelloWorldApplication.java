68b2f0c266667193fe22ee1421ca209813282d59
RuiRiya <109908012+RuiRiya@users.noreply.github.com>
2024-03-03 11:02:15 +0530

    Update again HelloWorldApplication.java

--- src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
@@ -23,7 +23,7 @@ public class HelloWorldApplication extends SpringBootServletInitializer {
 
     @RequestMapping("/")
     String helloWorld() {
-        return "Hello Class!";
+        return "Hii all....Let's learn Jenkins!";
     }
 
 }
